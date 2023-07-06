2d475efdd32b9d7c02094d5ba45646953fa592c1
Dmytro <dimores95@gmail.com>
2023-07-06 16:40:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let slider1 = new Swiper('.slider1', {
+    freeMode: true,
+    centeredSlides: true,
+    direction: 'vertical',
+    mousewheel: true,
+	slidesPerView: 1.75,
+	slidesOffsetBefore: -125
+});

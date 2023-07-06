d45f50571b728892558c3c40b3b14bc0b7a61639
Dmytro <dimores95@gmail.com>
2023-07-06 16:42:50 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-let slider1 = new Swiper('.slider1', {
-    freeMode: true,
-    centeredSlides: true,
-    direction: 'vertical',
-    mousewheel: true,
-	slidesPerView: 1.75,
-	slidesOffsetBefore: -125
+document.querySelectorAll('.slider').forEach((n, i) => {
+	window[`slider${i+1}`] = new Swiper(n, {
+		freeMode: true,
+		centeredSlides: true,
+		direction: 'vertical',
+		mousewheel: true,
+		slidesPerView: 1.75,
+		slidesOffsetBefore: -125
+	});
 });
+
+bindSwipers(slider1, slider2, slider3, slider4);
